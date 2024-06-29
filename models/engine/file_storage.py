01d59de376989106f61333c1b42be6a6648c7d98
rahmoncodes <abdulrahmonbb@gmail.com>
2024-06-29 09:06:35 +0100

    models/engine/file_storage.py, models/engine/__init__.py, models/__init__.py, models/base_model.py, tests/

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,49 +9,49 @@ import os
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """
     Represents the file storage for the project.
     """
+
     __file_path = "file.json"
     __objects = {}
-    CLASSES = {
-        "BaseModel": BaseModel
-    }
+    CLASSES = {"BaseModel": BaseModel}
 
     def all(self):
         """
         Returns the dictionary __objects.
         """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """
         Sets in __objects the obj with key <obj class name>.id
         """
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """
         Serializes __objects to the JSON file (path: __file_path)
         """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {key: obj.to_dict() for key, obj in FileStorage.__objects.items()},
-                f, indent=4
-            )
+        ser_obj = {}
+        for key, value in self.__objects.items():
+            ser_obj[key] = value.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(ser_obj, f)
 
     def reload(self):
         """
         Deserializes the JSON file to __objects (only if the JSON file exists)
         """
-        if os.path.exists(FileStorage.__file_path) and os.path.getsize(FileStorage.__file_path) > 0:
-            with open(FileStorage.__file_path, 'r') as f:
+        if os.path.exists(self.__file_path) and \
+                os.path.getsize(self.__file_path) > 0:
+            with open(self.__file_path, "r") as f:
                 content = json.load(f)
 
             for key, value in content.items():
                 cls_name = value["__class__"]
-                obj = FileStorage.CLASSES[cls_name](**value)
-                FileStorage.__objects[key] = obj
-
+                obj = self.CLASSES[cls_name](**value)
+                self.__objects[key] = obj
